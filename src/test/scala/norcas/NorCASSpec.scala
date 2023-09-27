10ac8e0824e381e344e0daad5bae12e548e8ca35
Hans Jakob Damsgaard <hansjakobdamsgaard@gmail.com>
2023-09-27 17:57:36 +0300

    finalize tests for NorCAS

--- src/test/scala/norcas/NorCASSpec.scala
+++ src/test/scala/norcas/NorCASSpec.scala
@@ -7,9 +7,11 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import approx.addition.{
-  Adder, FullAdder, AXA1, AXA2, AXA3, RCA, CLA, CSA, LOA, OFLOCA, GeAr
+  Adder, AdderIO, FullAdder, AXA1, AXA2, AXA3, RCA, CLA, CSA, LOA, OFLOCA, GeAr
 }
-import approx.multiplication.{Multiplier, Radix2Multiplier, RecursiveMultiplier}
+import approx.multiplication.{
+  Multiplier, MultiplierIO, Radix2Multiplier, RecursiveMultiplier
+}
 import approx.multiplication.comptree.{
   NoApproximation, ColumnTruncation, Miscounting, ORCompression, RowTruncation
 }
@@ -152,55 +154,55 @@ class StandaloneNorCASSpec extends NorCASSpec with Matchers {
 
   // Test Radix2Multiplier with RowTruncation(2)
   s"Radix2Multiplier$rtr1" should "generate error metrics" in {
-    test(new Radix2Multiplier(rtr1._1, approx=rtr1._2))
+    test(new Radix2Multiplier(Width, approx=rtr1._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with RowTruncation(4)
   s"Radix2Multiplier$rtr2" should "generate error metrics" in {
-    test(new Radix2Multiplier(rtr2._1, approx=rtr2._2))
+    test(new Radix2Multiplier(Width, approx=rtr2._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with ColumnTruncation(16)
   s"Radix2Multiplier$ctr1" should "generate error metrics" in {
-    test(new Radix2Multiplier(ctr1._1, approx=ctr1._2))
+    test(new Radix2Multiplier(Width, approx=ctr1._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with ColumnTruncation(32)
   s"Radix2Multiplier$ctr2" should "generate error metrics" in {
-    test(new Radix2Multiplier(ctr2._1, approx=ctr2._2))
+    test(new Radix2Multiplier(Width, approx=ctr2._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with ORCompression(16)
   s"Radix2Multiplier$cmp1" should "generate error metrics" in {
-    test(new Radix2Multiplier(cmp1._1, approx=cmp1._2))
+    test(new Radix2Multiplier(Width, approx=cmp1._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with ORCompression(32)
   s"Radix2Multiplier$cmp2" should "generate error metrics" in {
-    test(new Radix2Multiplier(cmp2._1, approx=cmp2._2))
+    test(new Radix2Multiplier(Width, approx=cmp2._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with Miscounting(16)
   s"Radix2Multiplier$msc1" should "generate error metrics" in {
-    test(new Radix2Multiplier(msc1._1, approx=msc1._2))
+    test(new Radix2Multiplier(Width, approx=msc1._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test Radix2Multiplier with Miscounting(32)
   s"Radix2Multiplier$msc2" should "generate error metrics" in {
-    test(new Radix2Multiplier(msc2._1, approx=msc2._2))
+    test(new Radix2Multiplier(Width, approx=msc2._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 
   // Test RecursiveMultiplier with approximation
   s"RecursiveMultiplier$kul" should "generate error metrics" in {
-    test(new RecursiveMultiplier(kul._1, kul._2))
+    test(new RecursiveMultiplier(Width, kul._2))
       .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
   }
 }
@@ -209,7 +211,324 @@ class CombinedNorCASSpec extends NorCASSpec with Matchers {
   import AdderConfigurations._
   import MultConfigurations._
 
-  ???
+  // Generate some random inputs to the adder and sample its registered outputs
+  def generateAdderMetrics[T <: AdderDUT](dut: T): Unit = {
+    val rng  = new scala.util.Random(42)
+
+    // Create an error reporter and track the sum output
+    val er = new ErrorReporter(
+      track(dut.io.sA, dut.io.sE, Metrics:_*)
+    )
+
+    // Apply NumTests random inputs and collect the outputs
+    (0 until NumTests).foreach { _ =>
+      dut.io.a.poke(BigInt(Width, rng).U)
+      dut.io.b.poke(BigInt(Width, rng).U)
+      dut.io.cin.poke(rng.nextBoolean().B)
+      dut.clock.step()
+      er.sample()
+    }
+
+    // Generate an error report and print it
+    println(er.report())
+  }
+
+  // Generic exact adder
+  class ExactAdder(width: Int) extends Module {
+    val io = IO(new AdderIO(width))
+    val sum  = Wire(UInt((width+1).W))
+    sum     := io.a +& io.b + io.cin
+    io.s    := sum(width-1, 0)
+    io.cout := sum(width)
+  }
+
+  // Common top-level DUT with exact adder implemented
+  abstract class AdderDUT(width: Int) extends Module {
+    val io = IO(new Bundle {
+      val a   = Input(UInt(width.W))
+      val b   = Input(UInt(width.W))
+      val cin = Input(Bool())
+      // Approximate outputs
+      val sA    = Output(UInt(width.W))
+      val coutA = Output(Bool())
+      // Exact outputs
+      val sE    = Output(UInt(width.W))
+      val coutE = Output(Bool())
+    })
+    def approxAdder: Module
+    val exactAdder = Module(new ExactAdder(width))
+    exactAdder.io.a   := io.a
+    exactAdder.io.b   := io.b
+    exactAdder.io.cin := io.cin
+    io.sE    := exactAdder.io.s
+    io.coutE := exactAdder.io.cout
+  }
+
+  // Test LOA
+  s"LOA$loa" should "generate error metrics" in {
+    class LOADUT extends AdderDUT(Width) {
+      val approxAdder = Module(new LOA(Width, loa._2))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new LOADUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test LAXA1
+  s"LAXA1$laxa1" should "generate error metrics" in {
+    class LAXADUT extends AdderDUT(Width) {
+      val approxAdder = Module(new LAXA(Width, laxa1._2, laxa1._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new LAXADUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test LAXA2
+  s"LAXA2$laxa2" should "generate error metrics" in {
+    class LAXADUT extends AdderDUT(Width) {
+      val approxAdder = Module(new LAXA(Width, laxa2._2, laxa2._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new LAXADUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test LAXA3
+  s"LAXA3$laxa3" should "generate error metrics" in {
+    class LAXADUT extends AdderDUT(Width) {
+      val approxAdder = Module(new LAXA(Width, laxa3._2, laxa3._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new LAXADUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test OFLOCAV1
+  s"OFLOCA$ofloca1" should "generate error metrics" in {
+    class OFLOCADUT extends AdderDUT(Width) {
+      val approxAdder = Module(new OFLOCA(Width, ofloca1._2, ofloca1._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new OFLOCADUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test OFLOCAV2
+  s"OFLOCA$ofloca2" should "generate error metrics" in {
+    class OFLOCADUT extends AdderDUT(Width) {
+      val approxAdder = Module(new OFLOCA(Width, ofloca2._2, ofloca2._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new OFLOCADUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test GeArV1 - needs redefinition as GeAr doesn't extend Adder
+  s"GeAr$gear1" should "generate error metrics" in {
+    class GeArDUT extends AdderDUT(Width) {
+      val approxAdder = Module(new GeAr(Width, gear1._2, gear1._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      approxAdder.io.ctrl.foreach(_ := false.B)
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new GeArDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Test GeArV2 - needs redefinition as GeAr doesn't extend Adder
+  s"GeAr$gear2" should "generate error metrics" in {
+    class GeArDUT extends AdderDUT(Width) {
+      val approxAdder = Module(new GeAr(Width, gear2._2, gear2._3))
+      approxAdder.io.a   := io.a
+      approxAdder.io.b   := io.b
+      approxAdder.io.cin := io.cin
+      approxAdder.io.ctrl.foreach(_ := false.B)
+      io.sA    := approxAdder.io.s
+      io.coutA := approxAdder.io.cout
+    }
+    test(new GeArDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateAdderMetrics(_))
+  }
+
+  // Generate some random inputs to the multiplier and sample its registered outputs
+  def generateMultMetrics[T <: MultiplierDUT](dut: T): Unit = {
+    val rng = new scala.util.Random(42)
+
+    // Create an error reporter and track the sum output
+    val er = new ErrorReporter(
+      track(dut.io.pA, dut.io.pE, Metrics:_*)
+    )
+
+    // Apply NumTests random inputs and collect the outputs
+    (0 until NumTests).foreach { _ =>
+      dut.io.a.poke(BigInt(Width, rng).U)
+      dut.io.b.poke(BigInt(Width, rng).U)
+      dut.clock.step()
+      er.sample()
+    }
+
+    // Generate an error report and print it
+    println(er.report())
+  }
+
+  // Generic exact multiplier
+  class ExactMultiplier(width: Int) extends Module {
+    val io = IO(new MultiplierIO(width))
+    io.p := io.a * io.b
+  }
+
+  // Common top-level DUT with exact multiplier implemented
+  abstract class MultiplierDUT(width: Int) extends Module {
+    val io = IO(new Bundle {
+      val a = Input(UInt(width.W))
+      val b = Input(UInt(width.W))
+      // Approximate output
+      val pA = Output(UInt((2*width).W))
+      // Exact output
+      val pE = Output(UInt((2*width).W))
+    })
+    def approxMult: Module
+    val exactMult = Module(new ExactMultiplier(width))
+    exactMult.io.a := io.a
+    exactMult.io.b := io.b
+    io.pE := exactMult.io.p
+  }
+
+  // Test Radix2Multiplier with RowTruncation(2)
+  s"Radix2Multiplier$rtr1" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=rtr1._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with RowTruncation(4)
+  s"Radix2Multiplier$rtr2" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=rtr2._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with ColumnTruncation(16)
+  s"Radix2Multiplier$ctr1" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=ctr1._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with ColumnTruncation(32)
+  s"Radix2Multiplier$ctr2" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=ctr2._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with ORCompression(16)
+  s"Radix2Multiplier$cmp1" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=cmp1._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with ORCompression(32)
+  s"Radix2Multiplier$cmp2" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=cmp2._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with Miscounting(16)
+  s"Radix2Multiplier$msc1" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=msc1._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test Radix2Multiplier with Miscounting(32)
+  s"Radix2Multiplier$msc2" should "generate error metrics" in {
+    class R2MultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new Radix2Multiplier(Width, approx=msc2._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new R2MultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
+
+  // Test RecursiveMultiplier with approximation
+  s"RecursiveMultiplier$kul" should "generate error metrics" in {
+    class RecMultDUT extends MultiplierDUT(Width) {
+      val approxMult = Module(new RecursiveMultiplier(Width, kul._2))
+      approxMult.io.a := io.a
+      approxMult.io.b := io.b
+      io.pA := approxMult.io.p
+    }
+    test(new RecMultDUT)
+      .withAnnotations(Seq(VerilatorBackendAnnotation, NoThreadingAnnotation))(generateMultMetrics(_))
+  }
 }
 
 class CMVMNorCASSpec extends NorCASSpec with Matchers {
